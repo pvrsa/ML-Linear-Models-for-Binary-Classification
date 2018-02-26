36d2f25adab553659ee7ebcff6f382476bbe5257
Sai Aasrith <sai.aasrith@gmail.com>
2018-02-26 22:45:09 +0530

    Still to find Inverse of Covariance :(

--- main.cpp
+++ main.cpp
@@ -12,8 +12,10 @@ void getInput(double X[][4],int Y[],int N,string lala);
 void getPriors(double* pC1,double* pC0);
 double PGM();
 vector<double> calcMean(int y);
-double calcVar(vector<double> mean,int y);
-double findLikelihood(int y,vector<double> mean,double covar,int row);
+vector<vector<double> > calcVar(vector<double> mean,int y);
+vector<vector<double> > addMatrix(vector<vector<double> > A,vector<vector<double> > B);
+
+double findLikelihood(int y,vector<double> mean,vector<vector<double> > covar,int row);
 
 
 const int Ntest=412;
@@ -63,31 +65,38 @@ double PGM(){
 	vector<double> mean0 = calcMean(0);
 	vector<double> mean1 = calcMean(1);
 
-	double var0 = calcVar(mean0,0)/(double)N;
-	double var1 = calcVar(mean0,1)/(double)N;
-	double covar = var0 + var1;
+	vector<vector<double> > var0 = calcVar(mean0,0);
+	vector<vector<double> > var1 = calcVar(mean1,1);
+	vector<vector<double> > covar = addMatrix(var0,var1);
+
+	for(int i = 0;i<4;i++){
+		for(int j=0;j<4;j++)
+			cout << covar[i][j] << " ";	
+		cout << endl;	
+	}
+
 
 	double pXC0,pXC1,a,b,ans;
 	int correst = 0;
-	for(int i=0;i<Ntest;i++){
+	// for(int i=0;i<Ntest;i++){
 
-		cout << i << endl;
+	// 	cout << i << endl;
 
-		pXC0 = findLikelihood(0,mean0,covar,i);	
-		pXC1 = findLikelihood(1,mean1,covar,i);
+	// 	pXC0 = findLikelihood(0,mean0,covar,i);	
+	// 	pXC1 = findLikelihood(1,mean1,covar,i);
 
-		a = pXC0*pC0;
-		b = pXC1*pC1;
-		ans = a/(a+b);
+	// 	a = pXC0*pC0;
+	// 	b = pXC1*pC1;
+	// 	ans = a/(a+b);
 
-		if(ans <= 0.5 && Ytest[i]){
-			correst++;
-		}else if(ans > 0.5 && Ytest[i]==0){
-			correst++;
-		}
+	// 	if(ans <= 0.5 && Ytest[i]){
+	// 		correst++;
+	// 	}else if(ans > 0.5 && Ytest[i]==0){
+	// 		correst++;
+	// 	}
 
 
-	}
+	// }
 	
 	cout << "ACCURACY = " << (double)correst/(double)Ntest << endl;
 	
@@ -133,40 +142,57 @@ vector<double> calcMean(int y){
 	
 }
 
-double calcVar(vector<double> mean,int y){
-	double ans=0;
+vector<vector<double> > calcVar(vector<double> mean,int y){
+	vector<vector<double> > ans(4,vector<double> (4,0.0));
 	double temp;
+	vector<double> lala(4,0.0);
 
 	for(int i=0;i<N;i++){
-		if(Y[i]==y){
-			for(int j=0;j<4;j++){
-				temp = X[i][j]-mean[j];
-				temp*=temp;
-				ans+=temp;
+		if(Y[i] == y){
+			for(int j = 0;j<4;j++){
+				lala[j] = X[i][j] - mean[j];
 			}
+
+			for(int j = 0; j<4 ; j++){
+				for(int k = 0; k<4; k++){
+					ans[j][k] += lala[j]*lala[k];
+				}
+			}
+
 		}
-		
 	}
 	return ans;
 }
 
-double findLikelihood(int y,vector<double> mean,double covar,int row){
-	double temp=0;
+double findLikelihood(int y,vector<double> mean,vector<vector<double> > covar,int row){
+	vector<double> temp;
 	for(int i=0;i<4;i++){
-
-		temp+=(Xtest[row][i]-mean[i])*(Xtest[row][i]-mean[i]);
+		temp.push_back(Xtest[row][i] - mean[i]);
 	}
 
-	temp/=(2*covar);
+
+
+	// temp/=(2*covar);
 
 	double a = 2*pi;
 	a*=a;
 
-	double b = sqrt(covar);
+	// double b = sqrt(covar);
 
-	double c = exp(temp);
+	// double c = exp(temp);
 
-	double ans = (1/(a*b*c));
+	double ans = (1/(a));
 
 	return ans;
+}
+
+vector<vector<double> > addMatrix(vector<vector<double> > A,vector<vector<double> > B){
+	
+	for(int i = 0;i<4;i++){
+		for(int j=0;j<4;j++){
+			A[i][j] += B[i][j];
+			A[i][j] /= N;
+		}
+	}
+return A;
 }
